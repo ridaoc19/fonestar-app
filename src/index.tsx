7ee4a04703dc1060f75918e004c66c3928e974b3
ricardo <ridaoc19@gmail.com>
2024-07-08 17:15:43 +0200

    chore: initial setup of dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import './index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <h1>app</h1>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<h1>app</h1>
+	</React.StrictMode>,
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,93 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.tsx'),
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/i,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: './',
-            },
-          },
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              webpackImporter: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|webp)$/i,
-        use: ['file-loader?name=assets/[name].[ext]', 'image-webpack-loader'],
-      },
-      {
-        test: /\.(woff)$/i,
-        use: ['file-loader?name=assets/[name].[ext]'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  devServer: {
-    client: {
-      progress: true,
-    },
-    static: path.resolve(__dirname, './dist'),
-    historyApiFallback: true,
-    open: true,
-    watchFiles: ['src/**/*.tsx', 'src/**/*.ts', 'public/**/*'],
-  },
+	entry: path.resolve(__dirname, './src/index.tsx'),
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/i,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/i,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: './',
+						},
+					},
+					'css-loader',
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							webpackImporter: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|webp)$/i,
+				use: ['file-loader?name=assets/[name].[ext]', 'image-webpack-loader'],
+			},
+			{
+				test: /\.(woff)$/i,
+				use: ['file-loader?name=assets/[name].[ext]'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new Dotenv(),
+		new MiniCssExtractPlugin(),
+	],
+	devServer: {
+		client: {
+			progress: true,
+		},
+		static: path.resolve(__dirname, './dist'),
+		historyApiFallback: true,
+		open: true,
+		watchFiles: ['src/**/*.tsx', 'src/**/*.ts', 'public/**/*'],
+	},
 };
